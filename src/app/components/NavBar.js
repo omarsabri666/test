aebac7de6f7eba841101581eb84d41bb1cfab2f6
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-06-24 06:28:43 +0300

    hi

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,37 @@
+// import Image from "next/image";
+// import Logo from "../../../public/Logo.svg"
+// import Hero from "../../../public/Video.png"
+
+// function NavBar() {
+//     return (
+//         <section className=" h-screen" style={{backgroundImage: `url(${Hero})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+//       <header className=" text-white  py-2">
+//         <nav className=" flex justify-around  items-center">
+//           <Image src={Logo} width={50} height={50} alt=" Logo" />
+//           <ul className=" flex items-center gap-5">
+//             <li>
+//               <a href="#About">About Us</a>
+//             </li>
+//             <li>
+//               <a href="#Services">Services</a>
+//             </li>
+//             <li>
+//               <a href="#News">News</a>
+//             </li>
+//             <li>
+//               <a href="#Careers">Careers</a>
+//             </li>
+//             <li>
+//                 <a href="#ContactUs">Contact Us</a>
+//             </li>
+//           </ul>
+//         </nav>
+//       </header>
+//       <main  >
+
+//       </main>
+//         </section>
+//     );
+// }
+
+// export default NavBar
